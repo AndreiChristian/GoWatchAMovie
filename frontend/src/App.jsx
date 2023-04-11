56a11e4ccab883e69044dc2047e161bf7da9fdbd
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-11 23:13:21 +0300

    added starting templates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <h1>App</h1>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go Watch A Movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#">
+            <span className="badge text-bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3" />
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+              <a href="#" className="list-group-item list-group-item-action">
+                Genres
+              </a>
+              <a href="#" className="list-group-item list-group-item-action">
+                Add Movie
+              </a>
+              <a href="#" className="list-group-item list-group-item-action">
+                Manage Catalog
+              </a>
+              <a href="#" className="list-group-item list-group-item-action">
+                GraphQL
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
